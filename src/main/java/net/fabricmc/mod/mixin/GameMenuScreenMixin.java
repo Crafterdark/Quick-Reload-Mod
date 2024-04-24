ff7d2d16ba90c752db1d6a3e1de1f9e74cffbc42
Crafterdark <118601009+Crafterdark@users.noreply.github.com>
2024-04-24 16:57:05 +0200

    Initial commit.

---
+++ src/main/java/net/fabricmc/mod/mixin/GameMenuScreenMixin.java
@@ -0,0 +1,11 @@
+package net.fabricmc.mod.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.client.gui.screen.GameMenuScreen;
+
+@Mixin(GameMenuScreen.class)
+public class GameMenuScreenMixin 
+{
+    
+}
