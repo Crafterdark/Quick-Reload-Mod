24b46aab1b054fa99e37dad8d5b694b65904de3e
Crafterdark <118601009+Crafterdark@users.noreply.github.com>
2024-05-10 12:26:21 +0200

    Fixed mod.

--- src/main/java/net/fabricmc/mod/mixin/GameMenuScreenMixin.java
+++ src/main/java/net/fabricmc/mod/mixin/GameMenuScreenMixin.java
@@ -1,14 +1,13 @@
 package net.fabricmc.mod.mixin;
 
+import net.minecraft.client.gui.screen.*;
+import net.minecraft.realms.RealmsBridge;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import net.fabricmc.mod.QuickReloadWaitingScreen;
-import net.minecraft.client.gui.screen.GameMenuScreen;
-import net.minecraft.client.gui.screen.Screen;
-import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.client.gui.screen.multiplayer.MultiplayerScreen;
 import net.minecraft.client.gui.widget.ButtonWidget;
 import net.minecraft.client.world.ClientWorld;
@@ -16,7 +15,7 @@ import net.minecraft.client.world.ClientWorld;
 import java.util.Objects;
 
 @Mixin(GameMenuScreen.class)
-public class GameMenuScreenMixin extends Screen
+public abstract class GameMenuScreenMixin extends Screen
 {
     @Inject(at = @At("TAIL"), method = "init()V")
 	private void init(CallbackInfo info) 
@@ -24,30 +23,68 @@ public class GameMenuScreenMixin extends Screen
 		this.buttons.add(new ButtonWidget(8, this.width / 2 - 100, this.height / 4 + 120 + 8, "Quick Reload"));
 	}
 
-    @Inject(at = @At("TAIL"), method = "buttonClicked()V")
-    private void buttonClick(ButtonWidget button, CallbackInfo info) {
+    @Override
+    public void buttonClicked(ButtonWidget button)
     {
-        if (button.id == 8)
-        {
-            boolean IWasServerIntegratedRunning = this.client.isIntegratedServerRunning();
-            button.active = false;
-
-            String levelName = Objects.requireNonNull(this.client.getServer()).getLevelName();
-
-            this.client.world.disconnect();
-            this.client.connect((ClientWorld)null);
-
-            QuickReloadWaitingScreen.SafeReloadAfterSavingChunks = false;
-
-            if (IWasServerIntegratedRunning)
-            {
-                this.client.setScreen(new QuickReloadWaitingScreen(levelName));
-            } 
-            else
-            {
+        switch (button.id) {
+            case 0: {
+                this.client.setScreen(new SettingsScreen(this, this.client.options));
+                break;
+            }
+            case 1: {
+                boolean bl = this.client.isIntegratedServerRunning();
+                boolean bl2 = this.client.isConnectedToRealms();
+                button.active = false;
+                this.client.world.disconnect();
+                this.client.connect(null);
+                if (bl) {
+                    this.client.setScreen(new TitleScreen());
+                    break;
+                }
+                if (bl2) {
+                    RealmsBridge realmsBridge = new RealmsBridge();
+                    realmsBridge.switchToRealms(new TitleScreen());
+                    break;
+                }
                 this.client.setScreen(new MultiplayerScreen(new TitleScreen()));
+                break;
+            }
+            case 4: {
+                this.client.setScreen(null);
+                this.client.closeScreen();
+                break;
+            }
+            case 5: {
+                this.client.setScreen(new AdvancementsScreen(this.client.player.networkHandler.method_14672()));
+                break;
+            }
+            case 6: {
+                this.client.setScreen(new StatsScreen(this, this.client.player.getStatHandler()));
+                break;
+            }
+            case 7: {
+                this.client.setScreen(new OpenToLanScreen(this));
+            }
+            case 8: {
+                boolean IWasServerIntegratedRunning = this.client.isIntegratedServerRunning();
+                button.active = false;
+
+                String levelName = Objects.requireNonNull(this.client.getServer()).getLevelName();
+
+                this.client.world.disconnect();
+                this.client.connect((ClientWorld)null);
+
+                QuickReloadWaitingScreen.SafeReloadAfterSavingChunks = false;
+
+                if (IWasServerIntegratedRunning)
+                {
+                    this.client.setScreen(new QuickReloadWaitingScreen(levelName));
+                }
+                else
+                {
+                    this.client.setScreen(new MultiplayerScreen(new TitleScreen()));
+                }
             }
         }
     }
-}
 }
